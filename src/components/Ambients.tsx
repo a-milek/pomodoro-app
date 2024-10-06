44ddcd45d777bfca336a46cbaf3949d1f7bd7f5f
a-milek <anna@milek.eu.org>
2024-10-06 17:23:45 +0200

    Video Skeletons

--- src/components/Ambients.tsx
+++ src/components/Ambients.tsx
@@ -1,20 +1,35 @@
-import { Box, Heading, SimpleGrid } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Heading,
+  SimpleGrid,
+} from "@chakra-ui/react";
 import YoutubeEmbed from "./YoutubeEmbed";
 import videoIds from "../configuration/videoIds.json"; // Import the JSON file
 
 const Ambients = () => {
   return (
-    <Box marginY={5} textAlign="left">
-      <Heading size="md" margin={3}>
-        {" "}
-        Ambients to help you focus
-      </Heading>
-      <SimpleGrid columns={4} spacing={10}>
-        {videoIds.map((id: string) => (
-          <YoutubeEmbed key={id} videoId={id} />
-        ))}
-      </SimpleGrid>
-    </Box>
+    <Accordion allowToggle>
+      <AccordionItem>
+        <AccordionButton>
+          <Heading size="md" margin={3} flex="1" textAlign="left">
+            Ambients to help you focus
+          </Heading>
+          <AccordionIcon marginLeft={3} />
+        </AccordionButton>
+
+        <AccordionPanel>
+          <SimpleGrid columns={[1, 2, 3, 4]} spacing={10}>
+            {videoIds.map((id: string) => (
+              <YoutubeEmbed key={id} videoId={id} />
+            ))}
+          </SimpleGrid>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
   );
 };
 
--- src/components/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed.tsx
@@ -1,13 +1,30 @@
-import { AspectRatio, Box } from "@chakra-ui/react";
+import { AspectRatio, Box, Skeleton } from "@chakra-ui/react";
+import { useState } from "react";
 
 const YoutubeEmbed = ({ videoId }: { videoId: string }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoaded = () => {
+    setIsLoading(false); // Set loading to false when the iframe has loaded
+  };
+
   return (
     <Box borderRadius="2xl" overflow="hidden">
       <AspectRatio maxW="560px" ratio={16 / 9}>
-        <iframe
-          title="YouTube video player"
-          src={`https://www.youtube.com/embed/${videoId}?modestbranding=1&rel=0&fs=0&color=white&disablekb=1" `}
-        ></iframe>
+        {isLoading ? (
+          <Skeleton>
+            <iframe
+              title="YouTube video player"
+              src={`https://www.youtube.com/embed/${videoId}?modestbranding=1&rel=0&fs=0&color=white&disablekb=1`}
+              onLoad={handleLoaded} // This triggers loading state change
+            />
+          </Skeleton>
+        ) : (
+          <iframe
+            title="YouTube video player"
+            src={`https://www.youtube.com/embed/${videoId}?modestbranding=1&rel=0&fs=0&color=white&disablekb=1`}
+          />
+        )}
       </AspectRatio>
     </Box>
   );
